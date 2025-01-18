c18cac22b2623e08662dc2938a0c9ce488f22cfc
Muhammad Taha <muhammad.taha@mail.mcgill.ca>
2025-01-17 21:10:47 -0500

    revert chunking

--- UserManualAgent.py
+++ UserManualAgent.py
@@ -2,13 +2,16 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.ollama import OllamaEmbedding
 
+import nest_asyncio
+nest_asyncio.apply()
+
 class UserManualAgent:
 
     def __init__(self):
         Settings.embed_model = OllamaEmbedding(model_name="nomic-embed-text")
         Settings.llm = Ollama(model="tinyllama", request_timeout=60)
-        Settings.chunk_size = 512
-        Settings.chunk_overlap = 50
+        # Settings.chunk_size = 512
+        # Settings.chunk_overlap = 50
         documents = SimpleDirectoryReader("docs/user_manuals").load_data(show_progress=True)
         index = VectorStoreIndex.from_documents(documents, show_progress=True)
         self.query_engine = index.as_query_engine()
