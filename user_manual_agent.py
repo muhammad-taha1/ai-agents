0b05d4736d1e2e856ce760196256b0ed77da3132
Muhammad Taha <muhammad.taha@mail.mcgill.ca>
2025-01-22 00:16:47 -0500

    test

--- user_manual_agent.py
+++ user_manual_agent.py
@@ -12,7 +12,7 @@ class UserManualAgent:
 
     def __init__(self):
         Settings.embed_model = OllamaEmbedding(model_name="all-minilm:22m")
-        Settings.llm = Ollama(model="tinyllama", request_timeout=60*5)
+        Settings.llm = Ollama(model="phi3:mini", request_timeout=60*5)
         Settings.chunk_size = 700
         Settings.chunk_overlap = 20
         documents = SimpleDirectoryReader("docs/user_manuals").load_data(show_progress=True)
