5cfcab885e6989180f0788b808a6d08bfbda3f42
Muhammad Taha <muhammad.taha@mail.mcgill.ca>
2025-01-23 22:19:26 -0500

    change to class

--- user_interface.py
+++ user_interface.py
@@ -1,45 +1,31 @@
 import streamlit as st
 from user_manual_agent import UserManualAgent
-import time
 
-
+class UserInterface:
+    def __init__(self):
+        self.user_manual_agent = UserManualAgent()
+        self.display_interface()
+    
 # Function to cycle through messages
-placeholder = st.empty()
+    def display_interface(self):
+        # Streamlit app
+        st.title("User Manual Agent")
+        st.write("Ask any questions you have about your appliances :sunglasses:")
 
-def cycle_messages(messages, delay=10):    
-    for message in messages:
-        placeholder.markdown(f"<h2 style='color:orange;'>{message}</h2>", unsafe_allow_html=True)
-        time.sleep(delay)
+        # Input text box for user query
+        user_query = st.text_input("Enter your question:")
 
-# Messages to cycle through
-messages = ["Waking up AI agent...", "Almost there...", "Just a moment..."]
-
-# Initialize the UserManualAgent with cycling messages
-delay = 1
-if 'agent' not in st.session_state:
-    with st.spinner(""):
-        cycle_messages(messages, delay)
-        delay = 10
-        st.session_state.agent = UserManualAgent()
-        # agent = UserManualAgent()
+        # Button to submit the query
+        if st.button("Submit"):
+            if user_query:
+                with st.spinner("Fetching response..."):
+                    response = st.session_state.agent.query(user_query)
+                    st.markdown("**Response**: \n\n")
+                    st.write(response.response_gen)
+                    
+            else:
+                st.write("Please enter a question.")
 
 
-placeholder.empty()  # Clear the placeholder
-
-# Streamlit app
-st.title("User Manual Agent")
-st.write("Ask any questions you have about your appliances :sunglasses:")
-
-# Input text box for user query
-user_query = st.text_input("Enter your question:")
-
-# Button to submit the query
-if st.button("Submit"):
-    if user_query:
-        with st.spinner("Fetching response..."):
-            response = st.session_state.agent.query(user_query)
-            st.markdown("**Response**: \n\n")
-            st.write(response.response_gen)
-            
-    else:
-        st.write("Please enter a question.")
+if __name__ == "__main__":
+    ui = UserInterface()
