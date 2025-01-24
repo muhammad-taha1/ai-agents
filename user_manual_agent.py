fe4b9f9edf077a677b05c6f43486714351324875
Muhammad Taha <muhammad.taha@mail.mcgill.ca>
2025-01-23 22:11:22 -0500

    convert to singleton

--- user_manual_agent.py
+++ user_manual_agent.py
@@ -14,59 +14,61 @@ import os
 load_dotenv()
 
 class UserManualAgent:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(UserManualAgent, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
 
     def __init__(self):
-        Settings.embed_model = OllamaEmbedding(model_name="all-minilm:22m")
-        # Settings.llm = Ollama(model="tinyllama", request_timeout=60*5)
-        llm = OpenRouter(
-            api_key=os.getenv("OPEN_ROUTER_KEY"),
-            model="meta-llama/llama-3.2-3b-instruct:free",
-            request_timeout=60*5
-        )
-
-        Settings.llm = llm
-
-        Settings.chunk_size = 700
-        Settings.chunk_overlap = 20
-        documents = SimpleDirectoryReader("docs/user_manuals").load_data(show_progress=True)
-        index = VectorStoreIndex.from_documents(documents, show_progress=True)
-        query_engine = index.as_query_engine(similarity_top_k=4)
-        user_manual_query_tool = QueryEngineTool.from_defaults(
-            query_engine, 
-            name="user_manual_query_tool",
-            description=(
-                "A RAG engine with of various user manuals of appliances.",
-                "Use a detailed plain text question as input to the tool.")
+        if not hasattr(self, 'initialized'):
+            Settings.embed_model = OllamaEmbedding(model_name="all-minilm:22m")
+            # Settings.llm = Ollama(model="tinyllama", request_timeout=60*5)
+            llm = OpenRouter(
+                api_key=os.getenv("OPEN_ROUTER_KEY"),
+                model="meta-llama/llama-3.2-3b-instruct:free",
+                request_timeout=60*5
             )
-        
-        system_prompt = """
-               System prompt:
 
-               You are an AI assistant specialized in answering questions about appliances using information from user manuals. 
+            Settings.llm = llm
 
-                1. Always rely on the **user_manual_query_tool** to retrieve information; do not use prior knowledge.
-                2. If a query cannot be answered using the tool, respond with: "I cannot find this information in the user manuals provided."
-                3. Do not attempt to generate answers without using the tool.
-                4. Keep responses concise and directly answer the query.
+            Settings.chunk_size = 700
+            Settings.chunk_overlap = 20
+            documents = SimpleDirectoryReader("docs/user_manuals").load_data(show_progress=True)
+            index = VectorStoreIndex.from_documents(documents, show_progress=True)
+            query_engine = index.as_query_engine(similarity_top_k=4)
+            user_manual_query_tool = QueryEngineTool.from_defaults(
+                query_engine, 
+                name="user_manual_query_tool",
+                description=(
+                    "A RAG engine with of various user manuals of appliances.",
+                    "Use a detailed plain text question as input to the tool.")
+                )
+            
+            system_prompt = """
+                System prompt:
 
-                Note: The tool name is **user_manual_query_tool**, and it should be used for all queries.
-            """
-        
-        self.agent = ReActAgent.from_tools(
-            tools=[user_manual_query_tool],
-            llm=Settings.llm,
-            verbose=True,
-            system_prompt=system_prompt,
-            )
-        
-        # print(self.agent.chat(system_prompt))
+                You are an AI assistant specialized in answering questions about appliances using information from user manuals. 
 
-        # message = ChatMessage(role="user", content="Tell me a joke")
-        # resp = llm.chat([message])
-        # print(resp)
+                    1. Always rely on the **user_manual_query_tool** to retrieve information; do not use prior knowledge.
+                    2. If a query cannot be answered using the tool, respond with: "I cannot find this information in the user manuals provided."
+                    3. Do not attempt to generate answers without using the tool.
+                    4. Keep responses concise and directly answer the query.
+
+                    Note: The tool name is **user_manual_query_tool**, and it should be used for all queries.
+                """
+            
+            self.agent = ReActAgent.from_tools(
+                tools=[user_manual_query_tool],
+                llm=Settings.llm,
+                verbose=True,
+                system_prompt=system_prompt,
+                )
+
         
-        
-        print("done initializing agent")
+            print("done initializing agent")
+            self.initialized = True
 
 
     def query(self, question):
