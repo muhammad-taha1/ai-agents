8ed5410c5b0b927e8a87cbccc6a75b7ca36581d2
Muhammad Taha <muhammad.taha@mail.mcgill.ca>
2025-01-23 22:29:31 -0500

    change to class

--- user_interface.py
+++ user_interface.py
@@ -1,31 +1,31 @@
 import streamlit as st
 from user_manual_agent import UserManualAgent
 
-class UserInterface:
-    def __init__(self):
-        self.user_manual_agent = UserManualAgent()
-        self.display_interface()
-    
+@st.cache_resource
+def initialize_agent():
+    user_manual_agent = UserManualAgent()
+    return user_manual_agent
+
+
 # Function to cycle through messages
-    def display_interface(self):
-        # Streamlit app
-        st.title("User Manual Agent")
-        st.write("Ask any questions you have about your appliances :sunglasses:")
+def display_interface(user_manual_agent):
+    # Streamlit app
+    st.title("User Manual Agent")
+    st.write("Ask any questions you have about your appliances :sunglasses:")
 
-        # Input text box for user query
-        user_query = st.text_input("Enter your question:")
+    # Input text box for user query
+    user_query = st.text_input("Enter your question:")
 
-        # Button to submit the query
-        if st.button("Submit"):
-            if user_query:
-                with st.spinner("Fetching response..."):
-                    response = self.user_manual_agent(user_query)
-                    st.markdown("**Response**: \n\n")
-                    st.write(response.response_gen)
-                    
-            else:
-                st.write("Please enter a question.")
+    # Button to submit the query
+    if st.button("Submit"):
+        if user_query:
+            with st.spinner("Fetching response..."):
+                response = user_manual_agent(user_query)
+                st.markdown("**Response**: \n\n")
+                st.write(response.response_gen)
+                
+        else:
+            st.write("Please enter a question.")
 
-
-if __name__ == "__main__":
-    ui = UserInterface()
+user_manual_agent = initialize_agent()
+display_interface(user_manual_agent)
